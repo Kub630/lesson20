280bd7af2a7e63f71e61c4da41ec8e8e0536f9f3
kuba <Kudaiberdigapyrov@gmail.com>
2023-03-07 16:21:36 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Method;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+   public static void method() {
+       lake();
+   }
+    public static void lake(){
+        System.out.println("Hello world!");
+    }
+
+}
